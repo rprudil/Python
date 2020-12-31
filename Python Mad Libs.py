29e5e2f3e90fbc8de30339d868415140cb6ad81b
rprudil <rprudil@gmail.com>
2020-12-31 12:08:31 -0800

    Initial commit

---
+++ Python Mad Libs.py
@@ -0,0 +1,29 @@
+#Mad Libs Generator
+
+adjective = input("Enter an adjective: ")
+adjective_2 = input("Enter an adjective: ")
+adjective_3 = input("Enter an adjective: ")
+
+name = input("Enter a name: ")
+name_2 = input("Enter a name: ")
+name_3 = input("Enter a name: ")
+noun = input("Enter a noun: ")
+noun_2 = input("Enter a noun: ")
+
+store = input("Enter a store name: ")
+verb = input("Enter an -ing verb: ")
+
+print("One morning, " + name + " went to " + store + ".")
+print("While at " + store + ", " +name + " saw a " + noun_2 + ".")
+print("The " + noun_2 + " was so " + adjective + " and " + adjective_2 + ".")
+print(name + " knew who to give this to for Christmas - " + name_2 + "!")
+print(name + " hurried home to give " + name_2 + " the " + noun_2 + ".")
+print("All the while, " + name_2 + " was waiting at the door, " + verb + ".")
+print("After what seemed like forever, " + name + " came into the driveway. ")
+print(name_2 + " was so excited!")
+print("The keyhole jiggled and finally, " + name + " was home!")
+print(name + " gave " + name_2 + " the " + noun_2 + " and " + name_2 + " did not know what to do with it... ")
+print(name_2 + " went upstairs into crawled into bed.")
+print(name + " was puzzled and did not know why " + name_2 + " did not want the " + noun_2 + ". After all, it was " +adjective + " and " + adjective_2 + ".")
+print(name + " took a closer look at the " + noun_2 + " and realized the " + noun_2 + " was not meant for creatures like " + name_2 + ".")
+print("It was meant for " + adjective_3 + " creatures like " + name_3 + ".")
